3f3710bf66fde7ef0088367868a42a23858c9c3f
Lud <instimila@gmail.com>
2022-07-08 23:15:46 +0200

    Atual

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 import { userSchema , loginSchema} from "../schemas/authSchema.js"
 import bcrypt from 'bcrypt';
 import { db } from "../dbStrategy/mongo.js";
+import {v4 as uuid} from "uuid"
 
 export async function createUser(req,res){
     const usuario=req.body
@@ -15,57 +16,59 @@ export async function createUser(req,res){
 
 try {
 
+  
         const emailExists=  await db.collection("users").findOne({email:usuario.email })
         if(emailExists){
             return res.status(409).send("Usuario ja cadastrado")
          
         }else{
-            await db.collection("users").insertOne({...usuario , password: passwordCripted })
+            await db.collection("users").insertOne({...usuario , password: passwordCripted})
           
             return  res.status(201).send("usuario criado com sucesso")
            
 
         }   
-    }
-    catch (error) {
+    
+    
+} catch (error) {
     console.log(error)
     return  res.send("deu ruim")
     
 }
 }
-
 export async function loginuser(req,res){
-  const usuario=req.body
-   
-    const { error } = loginSchema.validate(usuario) 
-    if (error) {
-        console.log(error)
-        return res.sendStatus(422)
-    
-    } 
-
-    try {
-
-    const userExist=  await db.collection("users").findOne({email:usuario.email  })
-
-  
-    if(userExist && bcrypt.compareSync(usuario.password , userExist.password)){
-
-        const token= uuid()
-        await db.collection('sessoes').insertOne({token , userId:userExist._id })
+    const usuario=req.body
+     
+      const { error } = loginSchema.validate(usuario) 
+      if (error) {
+          console.log(error)
+          return res.sendStatus(422)
       
+      } 
+  
+      try {
+  
+      const userExist=  await db.collection("users").findOne({email:usuario.email  })
+  
+    
+      if(userExist && bcrypt.compareSync(usuario.password , userExist.password)){
+  
+          const token= uuid()
+          await db.collection('sessoes').insertOne({token , userId:userExist._id })
         
-        return   res.status(201).send({token , userExist })
-
-        
-    }else{
-        return  res.status(401).send("Senha ou email incorretos")
-        
-
-    }    
-    } catch (error) {
-        console.log(error)
-        return res.send("deu ruim")
-    }
-
-}
+         
+          return   res.status(201).send({token , userExist })
+  
+          
+      }else{
+          return   res.status(401).send("Senha ou email incorretos")
+          
+  
+      }    
+      } catch (error) {
+          console.log(error)
+          return res.send(error)
+      }
+  
+  }
+  
